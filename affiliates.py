465f61ee08d0ea6c6cc3df8c6d0240039ead5e93
dneagu1 <dneagu@hbs.edu>
2020-11-13 22:17:26 -0500

    adding comments to py file

--- affiliates.py
+++ affiliates.py
@@ -4,7 +4,7 @@ import os
 import sqlite3
 import logging
 
-def check_integer(val):
+def check_integer(val): # Checks whether a string can be converted to an integer (CRD verification)
     try:
         val = int(val.strip())
     except:
@@ -12,43 +12,34 @@ def check_integer(val):
     return val
 
 def search_crd(txt,crd):
-    midx = txt.index('Organization Affiliates')
-    idxs = [i for i, x in enumerate(txt) if 'CRD #:' in x]
-    idxs = [item for item in idxs if item > midx]
-    # idxs = [int(txt.index(item)+1) for item in txt if 'CRD #:' in item]
+    midx = txt.index('Organization Affiliates') # Eliminate other affliate categories by starting at Org Affiliates section, which is last Affiliates section.
+    idxs = [i for i, x in enumerate(txt) if 'CRD #:' in x] # Get positions of "CRD #" string in text file.
+    idxs = [item for item in idxs if item > midx] # Keep only Org Affiliates
     fin_lst = []
     for step in range(len(idxs)):
         lst, idx  = [], idxs[step]
-
-        if isinstance(check_integer(txt[idx]),int):
+        if isinstance(check_integer(txt[idx]),int): # Keep text only if it is an integer/CRD #
             lst += [check_integer(txt[idx])]
-        # try:
         idx+= 1
-        next = check_integer(txt[idx])
-        while (':' not in str(next) or lst == []) and idx < idxs[step]+10:
+        next = check_integer(txt[idx]) # Check next row
+        while (':' not in str(next) or lst == []) and idx < idxs[step]+10: # Repeat process until a new data field is found or 10 rows are read.
             next = check_integer(txt[idx])
             if isinstance(next, int):
                 lst += [next]
             idx+= 1
             next = check_integer(txt[idx])
-        if lst == []:
-            print(next)
-            print('crd:{}'.format(crd))
-        # except Exception as e:
-        #     print(e)
         if len(lst) > 0:
-            # print(lst)
             fin_lst.append(max(lst))
     return fin_lst
 
 def db_init():
-    conn = sqlite3.connect('/home/anon/Documents/db/finra/subsidiaries.db')
+    conn = sqlite3.connect('/home/anon/Documents/db/finra/affiliates.db')
     cursor = conn.cursor()
     ################
     ## To delete DB
     ################
     try:
-        cursor.execute('DELETE FROM subs')
+        cursor.execute('DELETE FROM crd')
         conn.commit()
     except:
         pass
@@ -58,21 +49,26 @@ def db_init():
 
 
 if __name__=='__main__':
-    filedir ='/home/anon/Documents/db/finra/pdf'
+    filedir ='/home/anon/Documents/db/finra/pdf' # Enter correct directory where TXT Files are located
     files = [item for item in os.listdir(filedir) if '.txt' in item]
-    logging.basicConfig(filename='subsidiaries.log')
+    logging.basicConfig(filename='affiliates.log')
     conn = db_init()
     for file in tqdm(files):
         try:
+            ####################
+            ## Read TXT File
+            ####################
             crd = int(file.split('.')[0].replace('decr_',''))
             with open(filedir + '/' + file,'rb') as f:
                 txt = f.read()
             txt = txt.decode("utf-8").split('\n')
             txt = [item for item in txt if '©' not in item and 'User Guidance' not in item and 'Report about ' not in item and 'www.finra.org/brokercheck' not in item]
-            subsidiaries = search_crd(txt,crd)
-            frame = pd.DataFrame([[crd,sub] for sub in subsidiaries],columns=['crd','subsidiaries']).drop_duplicates()
-            frame.to_sql('subs',con=conn,if_exists='append',index=False)
-            # .drop_duplicates(reset_index=True)
+            ####################
+            ## Find Affiliates
+            ####################
+            affiliates = search_crd(txt,crd)
+            frame = pd.DataFrame([[crd,sub] for sub in affiliates],columns=['crd','affiliates']).drop_duplicates()
+            frame.to_sql('crd',con=conn,if_exists='append',index=False) # Upload to SQLite
             conn.commit()
-        except Exception as e:
+        except Exception as e: # Log errors by CRD #
             logging.critical('{}|{}'.format(crd,e))
